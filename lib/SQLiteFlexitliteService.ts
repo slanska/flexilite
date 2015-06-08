232d2a8f98eadee0ae183eaf0681a718c7ed1068
slanska <slan_ska@yahoo.com>
2015-06-07 22:37:43 -0400

    More changes

---
+++ lib/SQLiteFlexitliteService.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Ruslan Skorynin on 07.06.2015.
+ */
+
+    ///<refererence path="IFlexilite"/>
+
+export module Flexilite
+{
+    export class SQLiteService implements IFlexiliteService
+    {
+
+    }
+}
