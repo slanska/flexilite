67bfb255aa7695505ee589265a81fc2fe342fd5d
slanska <slan_ska@yahoo.com>
2017-01-22 10:36:01 -0500

    added dependencies

--- src/flexi/flexi_class_alter.c
+++ src/flexi/flexi_class_alter.c
@@ -2,3 +2,15 @@
 // Created by slanska on 2016-04-23.
 //
 
+/*
+ * Implementation of class alterration
+ */
+
+void flexi_class_alter(
+        const char *zClassName,
+        conat char *zNewClassDefJson
+)
+{
+
+}
+
--- src/flexi/flexi_query.c
+++ src/flexi/flexi_query.c
@@ -2,5 +2,12 @@
 // Created by slanska on 2017-01-21.
 //
 
-
+void flexi_query(
+        const char *zFrom,
+        const char *zWhere,
+        const char *zOrderBy,
+        const char *zSelect,
+        int skip,
+        int limit
+) {}
 
