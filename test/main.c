f697603c406cdd1725884106100c90d8d900b803
slanska <slan_ska@yahoo.com>
2017-05-06 14:01:55 -0400

    cmocka

--- test/main.c
+++ test/main.c
@@ -8,29 +8,9 @@
 
 #include <stdint.h>
 #include <string.h>
+#include <limits.h>
 #include <zconf.h>
 #include "definitions.h"
-
-/* A test case that does nothing and succeeds. */
-static void init_memory_db(void **state)
-{
-
-    struct sqlite3 *pDb;
-    db_open_in_memory(&pDb);
-    assert_non_null(pDb);
-    sqlite3_close(pDb);
-    pDb = NULL;
-}
-
-/* A test case that does nothing and succeeds. */
-static void init_db(void **state)
-{
-    struct sqlite3 *pDb;
-    db_create_or_open("../../data/test5.db", &pDb);
-    assert_non_null(pDb);
-    sqlite3_close(pDb);
-    pDb = NULL;
-}
 
 int main(int argc, char** argv)
 {
