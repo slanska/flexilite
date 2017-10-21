065de40afceed44acfc0fbcf1c8041bf21ebb0fa
slanska <slan_ska@yahoo.com>
2017-10-21 10:14:35 -0400

    first experiments with sqlite and duktape together

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,32 @@ void test_eval(duk_context *ctx, const char *code)
     }
 }
 
+class Stmt
+{
+public:
+    sqlite3 *db;            /* Database handle */
+    const char *zSql;       /* SQL statement, UTF-8 encoded */
+    sqlite3_stmt *pStmt = nullptr;  /* OUT: Statement handle */
+    const char *zTail = nullptr;     /* OUT: Pointer to unused portion of zSql */
 
+    explicit Stmt(uint64_t dbHandle, const char *_zSql) : db((sqlite3 *) dbHandle), zSql(_zSql)
+    {}
+    //    explicit Stmt(uintptr_t _db, const char* _zSql) : db((sqlite3 *) _db), zSql(_zSql)
+    //    {}
+
+    int Prepare()
+    {
+        int result = sqlite3_prepare(db, zSql, -1, &pStmt, &zTail);
+        return result;
+    }
+
+    ~Stmt()
+    {
+        sqlite3_free((void *) zSql);
+        sqlite3_free((void *) zTail);
+        sqlite3_finalize(pStmt);
+    }
+};
 
 extern "C" int sqlite3_extension_init(
         sqlite3 *db,
@@ -60,13 +85,23 @@ extern "C" int sqlite3_extension_init(
             // Register SQLite functions
             dukglue_register_function(pCtx, is_mod_2, "is_mod_2");
             dukglue_register_function(pCtx, sqlite3_libversion, "sqlite3_libversion");
+            dukglue_register_function(pCtx, sqlite3_memory_used, "sqlite3_memory_used");
+            dukglue_register_function(pCtx, sqlite3_memory_highwater, "sqlite3_memory_highwater");
+
+            dukglue_register_constructor<Stmt, uint64_t, const char *>(pCtx, "Stmt");
+            //            dukglue_register_constructor<Stmt, uintptr_t, const char*>(pCtx, "Stmt");
+            dukglue_register_method(pCtx, &Stmt::Prepare, "Prepare");
 
 
             test_eval(pCtx, "is_mod_2(5)");
 
             test_eval(pCtx, "sqlite3_libversion()");
             DukValue libVer = DukValue::take_from_stack(pCtx);
-            std::cout << libVer.as_c_string() << std::endl;
+            std::cout << "sqlite3_libversion: " << libVer.as_c_string() << std::endl;
+
+            test_eval(pCtx, "sqlite3_memory_used()");
+            DukValue memUsed = DukValue::take_from_stack(pCtx);
+            std::cout << "sqlite3_memory_used: " << memUsed.as_float() << std::endl;
 
 
             // Load .js
@@ -86,6 +121,16 @@ extern "C" int sqlite3_extension_init(
     };
 
     thread_local auto pDukCtx = std::unique_ptr<DukContext>(new DukContext());
+
+
+    auto pDb = reinterpret_cast<uint64_t >  (db);
+    dukglue_register_global(pDukCtx->getCtx(), pDb, "db");
+
+    std::string sql("    var st = new Stmt(db, 'select julianday();');"
+                            "st.Prepare();");
+    test_eval(pDukCtx->getCtx(), sql.c_str());
+    DukValue prepResult = DukValue::take_from_stack(pDukCtx->getCtx());
+    std::cout << "prepResult: " << prepResult.as_int() << std::endl;
 
     int (*funcs[])(sqlite3 *, char **, const sqlite3_api_routines *) = {
             eval_func_init,
